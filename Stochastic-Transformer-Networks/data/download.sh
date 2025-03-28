8390f8e16833fd88d35a31d174ac19a21027eb09
sajitheranda <118637181+sajitheranda@users.noreply.github.com>
2025-03-28 17:46:14 +0530

    change download details

--- Stochastic-Transformer-Networks/data/download.sh
+++ Stochastic-Transformer-Networks/data/download.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-wget "http://cihancamgoz.com/files/cvpr2020/phoenix14t.pami0.train"
-wget "http://cihancamgoz.com/files/cvpr2020/phoenix14t.pami0.dev"
-wget "http://cihancamgoz.com/files/cvpr2020/phoenix14t.pami0.test"
+# wget "http://cihancamgoz.com/files/cvpr2020/phoenix14t.pami0.train"
+# wget "http://cihancamgoz.com/files/cvpr2020/phoenix14t.pami0.dev"
+# wget "http://cihancamgoz.com/files/cvpr2020/phoenix14t.pami0.test"
