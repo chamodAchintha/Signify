ec752d7f66ba81a3141e530b751e5a8ad6a20e07
nilupul20 <nilupul.20@cse.mrt.ac.lk>
2025-05-11 08:31:51 +0530

    save translation model with bleu3 and blue2

--- Stochastic-Transformer-Networks/signjoey/sinhala_sentence/training.py
+++ Stochastic-Transformer-Networks/signjoey/sinhala_sentence/training.py
@@ -65,6 +65,7 @@ def train_translation_model(cfg_file: str):
 
     num_epochs = train_config["epochs"]
     best_bleu3 = 0.0  # Initialize best BLEU-3 score
+    best_bleu2 = 0.0
 
     logger.info(f"Number of trainable parameters = {sum(p.numel() for p in model.parameters() if p.requires_grad)}")
     logger.info("Training Starts...")
@@ -121,7 +122,7 @@ def train_translation_model(cfg_file: str):
             opened_file.write(f'Epoch [{epoch + 1}/{num_epochs}], Training Loss: {avg_train_loss:.4f} Validation Loss: {avg_val_loss:.4f}, lr: {current_lr:.6f}\n')
 
         # save checkpoint
-        if bleu_scores['bleu3'] >= best_bleu3:
+        if bleu_scores['bleu3'] > best_bleu3 or (bleu_scores['bleu3'] == best_bleu3 and bleu_scores['bleu2'] > best_bleu2):
             best_bleu3 = bleu_scores['bleu3']
             checkpoint = {
                 'epoch': epoch + 1,
@@ -129,10 +130,14 @@ def train_translation_model(cfg_file: str):
                 'optimizer_state_dict': optimizer.state_dict(),
                 'scheduler_state_dict': scheduler.state_dict(),
                 'best_bleu3': best_bleu3,
+                'best_bleu2': best_bleu2,
                 'learning_rate': optimizer.param_groups[0]['lr'],
             }
             torch.save(checkpoint, os.path.join(train_config["model_dir"], 'best_model.pth'))
-            logger.info(f"New best model saved with BLEU-3 score {best_bleu3:.4f}")
+            logger.info(f"New best model saved with BLEU-2 score {bleu_scores['bleu2']:.4f} BLEU-3 score {best_bleu3:.4f}")
+        
+        if bleu_scores['bleu2'] >= best_bleu2:
+            best_bleu2 = bleu_scores['bleu2']
 
         # Step the scheduler
         scheduler.step(avg_val_loss)
